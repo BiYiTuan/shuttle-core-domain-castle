20149f8485f5db2b3aeca07e6b9c58176eb4d592
Eben <me@ebenroux.co.za>
2014-11-15 09:33:03 +0200

    - initial commit

---
+++ Shuttle.Core.Domain.Castle/DomainEventDispatcher.cs
@@ -0,0 +1,25 @@
+﻿using Castle.Windsor;
+using Shuttle.Core.Infrastructure;
+
+namespace Shuttle.Core.Domain.Castle
+{
+    public class DomainEventDispatcher : IDomainEventDispatcher
+    {
+        private readonly IWindsorContainer container;
+
+        public DomainEventDispatcher(IWindsorContainer container)
+        {
+            Guard.AgainstNull(container, "container");
+
+            this.container = container;
+        }
+
+        public void Dispatch<T>(T @event) where T : IDomainEvent
+        {
+            foreach (var handler in container.ResolveAll<IDomainEventHandler<T>>())
+            {
+                handler.Handle(@event);
+            }
+        }
+    }
+}
